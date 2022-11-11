3fe702980736a788c659b646a877f316c94a437f
Yokozuna59 <u.yokozuna@gmail.com>
2022-11-12 01:23:12 +0300

    refactor: shorten module-info file by removing export line
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/module-info.java
+++ registration_assistant/src/main/java/module-info.java
@@ -1,9 +1,5 @@
-module registration_assistant {
+open module registration_assistant {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
-
-    opens registration_assistant to javafx.fxml;
-
-    exports registration_assistant;
 }
