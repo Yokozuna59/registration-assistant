fbe090bd26a1b11132e5bd53587fb3c3ae0142ba
ik1y <7amod.333@gmail.com>
2022-11-14 22:46:03 +0300

    create pane

--- registration_assistant/src/main/java/registration_assistant/App.java
+++ registration_assistant/src/main/java/registration_assistant/App.java
@@ -3,11 +3,16 @@ package registration_assistant;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
         stage.show();
     }
 
