54e2b31f4667825c72f7fea8c88fb80aff1d2553
Yokozuna59 <u.yokozuna@gmail.com>
2022-11-28 18:36:50 +0300

    chore: make app load index.fxml page
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/App.java
+++ registration_assistant/src/main/java/registration_assistant/App.java
@@ -3,18 +3,19 @@ package registration_assistant;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Button button = new Button("HELLO DOG");
-        Pane pane = new Pane(button);
-        Scene scene = new Scene(pane);
+        AnchorPane root = FXMLLoader.load(getClass().getClassLoader().getResource("views/index.fxml"));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.sizeToScene();
+        stage.setTitle("Registration Assistant");
         stage.show();
     }
 
