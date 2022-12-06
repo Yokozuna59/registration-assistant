f0f31fe5552398207b62b9502fc45a222a7322d7
banana0Boy <baboi3587@gmail.com>
2022-12-06 04:43:55 +0300

    make the remainCourses method

--- registration_assistant/src/main/java/registration_assistant/Student.java
+++ registration_assistant/src/main/java/registration_assistant/Student.java
@@ -18,4 +18,30 @@ public class Student {
     public ArrayList<Schedule> getSchedules() {
         return schedules;
     }
+
+    public void remainCourses(ArrayList<Course> courses, ArrayList<FinishedCourse> finishedCourses,
+            ArrayList<Section> sections) {
+        System.out.println(sections.size());
+        for (int i = 0; i < finisehdCourses.size(); i++) {
+            FinishedCourse passedCourse = finishedCourses.get(i);
+            for (int j = 0; j < courses.size(); j++) {
+                Course currentCourse = courses.get(j);
+
+                if (passedCourse.getCourse().equals(currentCourse.getName())) {
+                    courses.remove(currentCourse);
+                    break;
+                }
+            }
+            for (int j = 0; j < courses.size(); j++) {
+                Section coursePlace = sections.get(j);
+                if (passedCourse.getCourse().equals(coursePlace.getName())) {
+                    sections.remove(coursePlace);
+                }
+            }
+        }
+        System.out.println(courses.size());
+        System.out.println(sections.size());
+        // just for checking
+
+    }
 }
