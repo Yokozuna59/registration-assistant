24ffc27eff2f800c4078733857d6eec65cf31e24
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-09 16:47:14 +0300

    chore: add two columns to courses TableView: add and remove
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/App.java
+++ registration_assistant/src/main/java/registration_assistant/App.java
@@ -10,31 +10,29 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class App extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        ArrayList<Course> courses = ReadCSV.readDegreePlan(
-                getClass().getResource("/data/DegreePlan.csv").getFile());
-        ArrayList<FinishedCourse> finishedCourses = ReadCSV.readFinishedCourse(
-                getClass().getResource("/data/FinishedCourses.csv").getFile());
-        ArrayList<Section> sections = ReadCSV.readCourseOffering(
-                getClass().getResource("/data/CourseOffering.csv").getFile());
+        @Override
+        public void start(Stage stage) throws IOException {
+                ArrayList<Course> courses = ReadCSV.readDegreePlan(
+                                getClass().getResource("/data/DegreePlan.csv").getFile());
+                ArrayList<FinishedCourse> finishedCourses = ReadCSV.readFinishedCourse(
+                                getClass().getResource("/data/FinishedCourses.csv").getFile());
+                ArrayList<Section> sections = ReadCSV.readCourseOffering(
+                                getClass().getResource("/data/CourseOffering.csv").getFile());
 
-        Student student1 = new Student(finishedCourses, null);
-        student1.remainCourses(courses, sections);
-        System.out.println(sections.size());
-        for(int i=0;i<sections.size();i++)
-            System.out.println(sections.get(i).getFullName());
-        ScrollPane root = FXMLLoader.load(getClass().getClassLoader().getResource("views/index.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.setMinHeight(758);
-        stage.setMinWidth(1206);
-        stage.setTitle("Registration Assistant");
-        stage.show();
-    }
+                Student student = new Student(finishedCourses, null);
+                student.remainCourses(courses, finishedCourses, sections);
 
-    public static void main(String[] args) {
-        launch();
-    }
+                ScrollPane root = FXMLLoader.load(getClass().getClassLoader().getResource("views/index.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.sizeToScene();
+                stage.setMinHeight(758);
+                stage.setMinWidth(1447.2);
+                stage.setTitle("Registration Assistant");
+                stage.show();
+        }
+
+        public static void main(String[] args) {
+                launch();
+        }
 }
