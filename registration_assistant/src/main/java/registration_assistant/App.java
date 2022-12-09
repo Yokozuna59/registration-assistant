aa4f2a964eccf0ad26a183885021cf8a69965b1f
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-09 16:59:45 +0300

    style: change indentation from 4 to 8 spaces
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/App.java
+++ registration_assistant/src/main/java/registration_assistant/App.java
@@ -10,29 +10,29 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class App extends Application {
-        @Override
-        public void start(Stage stage) throws IOException {
-                ArrayList<Course> courses = ReadCSV.readDegreePlan(
-                                getClass().getResource("/data/DegreePlan.csv").getFile());
-                ArrayList<FinishedCourse> finishedCourses = ReadCSV.readFinishedCourse(
-                                getClass().getResource("/data/FinishedCourses.csv").getFile());
-                ArrayList<Section> sections = ReadCSV.readCourseOffering(
-                                getClass().getResource("/data/CourseOffering.csv").getFile());
+    @Override
+    public void start(Stage stage) throws IOException {
+        ArrayList<Course> courses = ReadCSV.readDegreePlan(
+                getClass().getResource("/data/DegreePlan.csv").getFile());
+        ArrayList<FinishedCourse> finishedCourses = ReadCSV.readFinishedCourse(
+                getClass().getResource("/data/FinishedCourses.csv").getFile());
+        ArrayList<Section> sections = ReadCSV.readCourseOffering(
+                getClass().getResource("/data/CourseOffering.csv").getFile());
 
-                Student student = new Student(finishedCourses, null);
-                student.remainCourses(courses, finishedCourses, sections);
+        Student student = new Student(finishedCourses, null);
+        student.remainCourses(courses, finishedCourses, sections);
 
-                ScrollPane root = FXMLLoader.load(getClass().getClassLoader().getResource("views/index.fxml"));
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.sizeToScene();
-                stage.setMinHeight(758);
-                stage.setMinWidth(1447.2);
-                stage.setTitle("Registration Assistant");
-                stage.show();
-        }
+        ScrollPane root = FXMLLoader.load(getClass().getClassLoader().getResource("views/index.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.setMinHeight(758);
+        stage.setMinWidth(1447.2);
+        stage.setTitle("Registration Assistant");
+        stage.show();
+    }
 
-        public static void main(String[] args) {
-                launch();
-        }
+    public static void main(String[] args) {
+        launch();
+    }
 }
