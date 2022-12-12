0c77493cbd4b41474b03721037a184e4640cc807
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-12 19:48:48 +0300

    chore: create not finished reRenderSchedule method
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/Schedule.java
+++ registration_assistant/src/main/java/registration_assistant/Schedule.java
@@ -1,7 +1,15 @@
 package registration_assistant;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import registration_assistant.controllers.SectionController;
 
 public class Schedule implements Serializable {
     private String term;
@@ -40,5 +48,33 @@ public class Schedule implements Serializable {
 
     public ArrayList<Section> getSections() {
         return sections;
+    }
+
+    public void reRenderSchedule(ScrollPane scrollPane) throws IOException {
+        Map<String, Integer> days = new HashMap<>();
+        days.put("U", 1);
+        days.put("M", 2);
+        days.put("T", 3);
+        days.put("W", 4);
+        days.put("R", 5);
+
+        for (Section section : sections) {
+            for (String day : section.getDays().split("")) {
+                FXMLLoader loader = new FXMLLoader();
+                Pane pane = loader.load(getClass().getResource("/views/section.fxml"));
+                SectionController controller = loader.getController();
+
+                String[] time = section.getTime().split("-");
+                String start = time[0];
+                String end = time[1];
+                controller.setStart(start);
+                controller.setEnd(end);
+                controller.setInstructor(section.getInstructor());
+                controller.setSize(0);
+
+                // pane.setLayoutY(0);
+                // pane.setLayoutX(220.5 + days.get(day) * 225);
+            }
+        }
     }
 }
