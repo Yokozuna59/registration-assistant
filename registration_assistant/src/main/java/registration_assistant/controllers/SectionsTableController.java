d2a69f6e6210972ee5b8a8e1ba4d86da9fe11f7e
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-13 02:06:10 +0300

    feat: make sections table controller render sections data
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

---
+++ registration_assistant/src/main/java/registration_assistant/controllers/SectionsTableController.java
@@ -0,0 +1,75 @@
+package registration_assistant.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import registration_assistant.App;
+import registration_assistant.Section;
+
+public class SectionsTableController implements Initializable {
+    @FXML
+    private TableView<Section> table;
+
+    @FXML
+    private TableColumn<Section, String> courseName;
+
+    @FXML
+    private TableColumn<Section, String> activity;
+
+    @FXML
+    private TableColumn<Section, String> crn;
+
+    @FXML
+    private TableColumn<Section, String> description;
+
+    @FXML
+    private TableColumn<Section, String> instructor;
+
+    @FXML
+    private TableColumn<Section, String> days;
+
+    @FXML
+    private TableColumn<Section, String> time;
+
+    @FXML
+    private TableColumn<Section, String> place;
+
+    @FXML
+    private TableColumn<Section, String> status;
+
+    @FXML
+    private TableColumn<Section, String> waitlist;
+
+    @FXML
+    private TableColumn<Button, String> add;
+
+    @FXML
+    private TableColumn<Button, String> remove;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<Section> list = FXCollections.observableArrayList(App.getSections());
+        courseName.setCellValueFactory(new PropertyValueFactory<Section, String>("fullName"));
+        activity.setCellValueFactory(new PropertyValueFactory<Section, String>("activity"));
+        crn.setCellValueFactory(new PropertyValueFactory<Section, String>("crn"));
+        description.setCellValueFactory(new PropertyValueFactory<Section, String>("description"));
+        instructor.setCellValueFactory(new PropertyValueFactory<Section, String>("instructor"));
+        days.setCellValueFactory(new PropertyValueFactory<Section, String>("days"));
+        time.setCellValueFactory(new PropertyValueFactory<Section, String>("time"));
+        place.setCellValueFactory(new PropertyValueFactory<Section, String>("location"));
+        status.setCellValueFactory(new PropertyValueFactory<Section, String>("status"));
+        waitlist.setCellValueFactory(new PropertyValueFactory<Section, String>("waitlist"));
+        // add.setCellValueFactory(new PropertyValueFactory<Section, Button>(""));
+        // remove.setCellValueFactory(new PropertyValueFactory<Section,
+        // Button>("remove"));
+        table.setItems(list);
+    }
+}
