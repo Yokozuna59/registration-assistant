b2c3b1160e0d311b309ed82c715791aa516cc22b
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-17 03:19:53 +0300

    chore: make IndexController setPane
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/controllers/IndexController.java
+++ registration_assistant/src/main/java/registration_assistant/controllers/IndexController.java
@@ -1,27 +1,39 @@
 package registration_assistant.controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import registration_assistant.App;
 
 public class IndexController implements Initializable {
     public void switchToBasket(ActionEvent e) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/basket.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        App.setPane(root);
     }
 
     public void saveSchedule() {
+        File studentSchedule = Paths.get("src", "main", "resources", "student",
+                "schedule.ser").toFile();
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(studentSchedule);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(App.getStudent().getSchedules());
+            objectOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
