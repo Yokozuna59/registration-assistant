9852e5a0e9c070910b00a25856d510117f127c75
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-17 17:31:30 +0300

    chore: verify that section isn't in basket before
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/Basket.java
+++ registration_assistant/src/main/java/registration_assistant/Basket.java
@@ -11,7 +11,16 @@ public class Basket implements Serializable {
     }
 
     public void addSection(Section section) {
-        sectionsBasket.add(section);
+        boolean found = false;
+        for (Section section2 : sectionsBasket) {
+            if (section.getCrn().equals(section2.getCrn())
+                    && section.getActivity().equals(section2.getActivity())) {
+                found = true;
+            }
+        }
+        if (!found) {
+            sectionsBasket.add(section);
+        }
     }
 
     public void removeSection(Section section) {
