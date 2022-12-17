e8a5d4d710fa4623f93f5c4605c43283dd590b55
Yokozuna59 <u.yokozuna@gmail.com>
2022-12-17 03:19:33 +0300

    feat: make scene static variable and create setPane method
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- registration_assistant/src/main/java/registration_assistant/App.java
+++ registration_assistant/src/main/java/registration_assistant/App.java
@@ -1,23 +1,34 @@
 package registration_assistant;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     private static ArrayList<Section> sections;
     private static Student student;
+    private static Scene scene;
+
+    public static void setPane(Object pane) {
+        if (pane instanceof Pane)
+            App.scene.setRoot((Pane) pane);
+        else if (pane instanceof ScrollPane)
+            App.scene.setRoot((ScrollPane) pane);
+    }
+
+    public static Scene getScene() {
+        return scene;
+    }
 
     public static Student getStudent() {
         return student;
@@ -35,22 +46,10 @@ public class App extends Application {
                 getClass().getResource("/data/FinishedCourses.csv").getFile());
         sections = ReadCSV.readCourseOffering(
                 getClass().getResource("/data/CourseOffering.csv").getFile());
-        Schedule schedule;
 
         File studentFolder = Paths.get("src", "main", "resources", "student").toFile();
-        if (studentFolder.exists()) {
-            FileInputStream fileInputStream = new FileInputStream(
-                    getClass().getResource("/student/schedule.ser").getFile());
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            schedule = (Schedule) objectInputStream.readObject();
-        } else {
-            studentFolder.mkdir();
-            schedule = new Schedule("202220", new ArrayList<Section>());
-
-            FileOutputStream fileOutputStream = new FileOutputStream(new File(studentFolder, "schedule.ser"));
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(schedule);
-        }
+        studentFolder.mkdir();
+        Schedule schedule = new Schedule("202220", new ArrayList<Section>());
 
         student = new Student(finishedCourses, schedule, new Basket(new ArrayList<>()));
         for (int i = 0; i < sections.size(); i++) {
@@ -60,7 +59,7 @@ public class App extends Application {
         student.remainCourses(courses, finishedCourses, sections);
 
         AnchorPane root = FXMLLoader.load(getClass().getClassLoader().getResource("views/basket.fxml"));
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.sizeToScene();
         stage.setMinWidth(1563.05);
